5db7abc3eabf96a3d310ecf3f3ea103454a5d2e3
Emanuel Peire <emapeire@outlook.com>
2023-10-18 11:48:17 -0300

    add reset input value

--- app/actions.ts
+++ app/actions.ts
@@ -1,8 +1,8 @@
-'use server'
+"use server";
 
-import { revalidatePath } from 'next/cache'
-import { sql } from '@vercel/postgres'
-import { z } from 'zod'
+import { revalidatePath } from "next/cache";
+import { sql } from "@vercel/postgres";
+import { z } from "zod";
 
 // CREATE TABLE todos (
 //   id SERIAL PRIMARY KEY,
@@ -12,21 +12,21 @@ import { z } from 'zod'
 export async function createTodo(prevState: any, formData: FormData) {
   const schema = z.object({
     todo: z.string().min(1),
-  })
+  });
   const data = schema.parse({
-    todo: formData.get('todo'),
-  })
+    todo: formData.get("todo"),
+  });
 
   try {
     await sql`
     INSERT INTO todos (text)
     VALUES (${data.todo})
-  `
+  `;
 
-    revalidatePath('/')
-    return { message: `Added todo ${data.todo}` }
+    revalidatePath("/");
+    return { message: `Added todo ${data.todo}`, resetInput: true };
   } catch (e) {
-    return { message: 'Failed to create todo' }
+    return { message: "Failed to create todo", resetInput: false };
   }
 }
 
@@ -34,21 +34,21 @@ export async function deleteTodo(prevState: any, formData: FormData) {
   const schema = z.object({
     id: z.string().min(1),
     todo: z.string().min(1),
-  })
+  });
   const data = schema.parse({
-    id: formData.get('id'),
-    todo: formData.get('todo'),
-  })
+    id: formData.get("id"),
+    todo: formData.get("todo"),
+  });
 
   try {
     await sql`
       DELETE FROM todos
       WHERE id = ${data.id};
-    `
+    `;
 
-    revalidatePath('/')
-    return { message: `Deleted todo ${data.todo}` }
+    revalidatePath("/");
+    return { message: `Deleted todo ${data.todo}` };
   } catch (e) {
-    return { message: 'Failed to delete todo' }
+    return { message: "Failed to delete todo" };
   }
 }
--- app/add-form.tsx
+++ app/add-form.tsx
@@ -4,6 +4,7 @@
 import { experimental_useFormState as useFormState } from 'react-dom'
 import { experimental_useFormStatus as useFormStatus } from 'react-dom'
 import { createTodo } from '@/app/actions'
+import { useEffect, useState } from 'react'
 
 const initialState = {
   message: null,
@@ -21,11 +22,25 @@ function SubmitButton() {
 
 export function AddForm() {
   const [state, formAction] = useFormState(createTodo, initialState)
+  const [inputValue, setInputValue] = useState('');
+
+  useEffect(() => {
+    if (state?.resetInput) {
+      setInputValue('');
+    }
+  }, [state]);
 
   return (
     <form action={formAction}>
       <label htmlFor="todo">Enter Task</label>
-      <input type="text" id="todo" name="todo" required />
+      <input
+        type="text"
+        id="todo"
+        name="todo"
+        required
+        value={inputValue}
+        onChange={(event) => setInputValue(event.target.value)}
+      />
       <SubmitButton />
       <p aria-live="polite" className="sr-only" role="status">
         {state?.message}
