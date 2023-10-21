1cf8ecf1ef42be66d77c9be7de9369ed778718e4
Emanuel Peire <emapeire@outlook.com>
2023-10-20 21:05:29 -0300

    global css fixed

--- app/components/card.tsx
+++ app/components/card.tsx
@@ -4,13 +4,13 @@ import { DeleteForm } from "../delete-form";
 
 export default function CardComponet({ id, todo }: FormProps) {
   return (
-    <>
+    <div>
       <section>
         <EditForm id={id} todo={todo} />
       </section>
       <section>
         <DeleteForm id={id} todo={todo} />
       </section>
-    </>
+    </div>
   );
 }
